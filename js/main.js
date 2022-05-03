9b979cd7736df275f9e653e8bda0a31a24dbc2bd
alfich <98544885+alfich@users.noreply.github.com>
2022-05-03 13:16:11 -0400

    zoom

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ $( document ).ready(function() {
             panzoom.zoom(4, { animate: true });
           } else if (it_id == 9) {
             document.getElementById("item-9").classList.add('visible');
-            panzoom.pan(-30, 70);
+            panzoom.pan(-40, 80);
             panzoom.zoom(12, { animate: true });
           } 
 
